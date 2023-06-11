a274600f6c79d4413c363879b63b2e28f994a741
jaehyeok <jhlim244@naver.com>
2023-06-11 13:07:44 +0900

    fix

--- src/Controller.h
+++ src/Controller.h
@@ -1,31 +1,24 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#include "Database.h" 
+#include "Database.h"
 #include "View.h"
 
 class Controller
 {
 public:
     // 생성자: Database 객체와 View 객체를 인자로 받음
-    Controller(Database* db, View* v);
+    Controller(Database *db, View *v);
 
     // 주 로직을 실행하는 메소드
     void run();
 
 private:
-    Database* database;  // Database 객체를 가리키는 포인터
-    View* view; // View 객체를 가리키는 포인터
-
-    // 차량 입차를 처리하는 메소드
-    void enterCar();
-
-    // 차량 출차를 처리하는 메소드
-    void exitCar();
-
-    // 데이터를 관리하는 메소드
-    void manageData();
-    void calculate();
+    Database *database; // Database 객체를 가리키는 포인터
+    View *view;         // View 객체를 가리키는 포인터
+    void enterCar();    // 차량 입차를 처리하는 메소드
+    void calculate();   // 차량 출차 시 요금 계산
+    void manageData();  // 데이터를 관리하는 메소드
 };
 
 #endif
